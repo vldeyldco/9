38ff36ba62403ba8a4768b64818fb8e3683fe7f9
vldeyldco <randiii1507@gmail.com>
2026-06-12 15:05:35 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function joinWhatsAppGroup() {
 }
 
 function openFacebookPage() {
-    window.open("https://www.facebook.com/share/g/1BJTvJ1xmx/");
+    window.open("https://www.facebook.com/profile.php?id=61584495795792");
 }
 const video = document.getElementById("video");
 const overlay = document.getElementById("videoOverlay");
