e4065e31d1fd008f315d1f5f91a7de1d2883b4d3
vldeyldco <randiii1507@gmail.com>
2026-06-20 20:19:06 +0700

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function joinWhatsAppGroup() {
 }
 
 function openFacebookPage() {
-    window.open("https://www.facebook.com/profile.php?id=61588943410089");
+    window.open("https://facebook.com/groups/1646174380211584/");
 }
 const video = document.getElementById("video");
 const overlay = document.getElementById("videoOverlay");
